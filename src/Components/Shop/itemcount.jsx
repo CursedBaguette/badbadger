e84e67b77fa553e7e62a6670ab577c1e1dc83adc
Roberto <sexyskavenlover@gmail.com>
2021-09-21 20:51:03 -0300

    b

---
+++ src/Components/Shop/itemcount.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const ItemCount = ({ initial, onAdd, test123 }) => {
+  const [count, setCount] = useState(initial);
+
+  function sumar() {
+    if (count < 12) {
+      setCount(count + 1);
+    } else {
+      alert("cantidad maxima de productos");
+    }
+  }
+
+  function restar() {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  }
+
+  /** Funcion que debe editarse para exportar la cantidad al carrrito **/
+  function onAdd(contar) {
+    alert(`${contar * test123.precio} Pesos`);
+  }
+  /** Funcion que debe editarse para exportar la cantidad al carrrito **/
+
+  const agregarCarrito = () => {
+    onAdd(count);
+  };
+
+  var precioAB = count * test123.precio;
+
+  return (
+    <div>
+      <button onClick={restar} class="btn btn-dark">
+        -
+      </button>
+      <label className="titulos123 ">{count}</label>
+      <button onClick={sumar} class="btn btn-dark">
+        +
+      </button>
+
+      <Link to="/cart/">
+        <button onClick={agregarCarrito} className="titulos123 btn btn-dark">
+          Agregar{" "}
+        </button>
+      </Link>
+      <div className="precio1">$ {precioAB} Pesos</div>
+    </div>
+  );
+};
+
+export default ItemCount;
---
+++ src/Components/context/webcontext.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function webcontext() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default  webcontext;
